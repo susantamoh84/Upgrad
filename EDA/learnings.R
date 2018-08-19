c127cec485c6897b79fef16f51ad3689d067d4ef
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-08-19 11:40:09 +0530

    Add files via upload

---
+++ EDA/learnings.R
@@ -0,0 +1,15 @@
+odi$new_variable<- ifelse(odi$Runs>=100,1,0) 
+
+Taking out highly correlated variables: Take ratio of two or percentages
+
+New Columns Science/Process:
+
+- From Type of column extract its attributes
+	-e.g From Date, Day, months, years, hour of day, day of week, day of month etc
+- From Business Domain understanding
+	-pass criteria, target, 
+- Purely From Data
+	-take ratio between 2 highly corr values
+	-introduce a business formula business driver/data driven
+	-aggregate several column
+	-Column - result of clustering/classification/outlier detection
