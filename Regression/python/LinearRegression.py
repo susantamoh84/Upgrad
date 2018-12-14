9e7574f3579c61ccc2649fd552b1167028d6a253
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-12-14 12:40:34 +0530

    Update LinearRegression.py

--- Regression/python/LinearRegression.py
+++ Regression/python/LinearRegression.py
@@ -90,4 +90,70 @@ r_squared
 plt.scatter(X_test, y_test)
 plt.plot(X_test, 6.948 + 0.054 * X_test, 'r')
 plt.show()
-plt.show()
+
+from sklearn.model_selection import train_test_split
+X_train_lm, X_test_lm, y_train_lm, y_test_lm = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 100)
+X_train_lm.shape
+
+print(X_train.head())
+print(X_train_lm[:5,])
+
+X_train_lm = X_train_lm.reshape(-1,1)
+X_test_lm = X_test_lm.reshape(-1,1)
+
+print(X_train_lm[:5,])
+
+print(X_train_lm.shape)
+print(y_train_lm.shape)
+print(X_test_lm.shape)
+print(y_test_lm.shape)
+
+from sklearn.linear_model import LinearRegression
+
+# Representing LinearRegression as lr(Creating LinearRegression Object)
+lm = LinearRegression()
+
+# Fit the model using lr.fit()
+lm.fit(X_train_lm, y_train_lm)
+
+print(lm.intercept_)
+print(lm.coef_)
+
+corrs = np.corrcoef(X_train, y_train)
+print(corrs)
+
+corrs[0,1] ** 2
+
+############ Scaling the variables ##############
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.7, test_size = 0.3, random_state = 100)
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# One aspect that you need to take care of is that the 'fit_transform' can be performed on 2D arrays only. So you need to
+# reshape your 'X_train_scaled' and 'y_trained_scaled' data in order to perform the standardisation.
+X_train_scaled = X_train.reshape(-1,1)
+y_train_scaled = y_train.reshape(-1,1)
+
+X_train_scaled.shape
+
+# Create a scaler object using StandardScaler()
+scaler = StandardScaler()
+#'Fit' and transform the train set; and transform using the fit on the test set later
+X_train_scaled = scaler.fit_transform(X_train_scaled)
+y_train_scaled = scaler.fit_transform(y_train_scaled)
+
+print("mean and sd for X_train_scaled:", np.mean(X_train_scaled), np.std(X_train_scaled))
+print("mean and sd for y_train_scaled:", np.mean(y_train_scaled), np.std(y_train_scaled))
+
+# Let's fit the regression line following exactly the same steps as done before
+X_train_scaled = sm.add_constant(X_train_scaled)
+
+lr_scaled = sm.OLS(y_train_scaled, X_train_scaled).fit()
+
+# Check the parameters
+lr_scaled.params
+
+print(lr_scaled.summary())
+
+
